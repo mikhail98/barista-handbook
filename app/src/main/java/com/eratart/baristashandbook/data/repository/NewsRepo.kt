48731574f47f38e5a3876f5b6b8d051a6d252572
Mikhail Yarashevich <mikhailyarashevich@Mikhails-MacBook-Pro.local>
2022-05-22 01:50:27 +0200

    all drinks inside

--- app/src/main/java/com/eratart/baristashandbook/data/repository/NewsRepo.kt
+++ app/src/main/java/com/eratart/baristashandbook/data/repository/NewsRepo.kt
@@ -3,7 +3,6 @@ package com.eratart.baristashandbook.data.repository
 import com.eratart.baristashandbook.core.ext.printError
 import com.eratart.baristashandbook.data.mapper.repo.NewsFromBotMapper
 import com.eratart.baristashandbook.data.model.NewsBotResponse
-import com.eratart.baristashandbook.data.model.TelegramUpdatesResponse
 import com.eratart.baristashandbook.data.network.api.TgApi
 import com.eratart.baristashandbook.domain.exceptions.ThrowableTransformer
 import com.eratart.baristashandbook.domain.model.NewsBot
@@ -38,7 +37,7 @@ class NewsRepo(private val tgApi: TgApi) : INewsRepo {
                             e.printError()
                         }
                     }
-                news
+                news.sortedBy { item -> item.date }.asReversed()
             }
             .catch { error ->
                 throw ThrowableTransformer.transform(error)
